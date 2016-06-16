2f530311a65ee9e782404ae28e55225a6d90652f
Lucas Possatti <lucas_possatti@hotmail.com>
2016-06-16 11:06:22 -0300

    Put rules for git to ignore some files globally

--- git-setup.sh
+++ git-setup.sh
@@ -22,3 +22,9 @@ git config --global alias.dc 'diff --cached'
 git config --global alias.lg "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 git config --global alias.l '!git lg'
 git config --global alias.hist 'log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
+
+# Ignore
+mkdir -p "$HOME/.config/git"
+echo 'nohup.out' >> $HOME/.config/git/ignore
+echo '*.bkp' >> $HOME/.config/git/ignore
+echo '*.log' >> $HOME/.config/git/ignore
