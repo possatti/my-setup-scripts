f6624efee9c22d843c0159d11acac10ce0b5703b
Lucas Possatti <lucas_possatti@hotmail.com>
2016-06-22 13:30:35 -0300

    Install nodejs-legacy together with nodejs

--- nodejs.sh
+++ nodejs.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 sudo apt-get -y install nodejs
+sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install npm
-echo 'alias node=nodejs' >> $HOME/.bashrc
-fish -c 'alias node nodejs; funcsave node'
