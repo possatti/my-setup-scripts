943882f0e47ee52a1f82e4e463f47c1131a98368
Lucas Possatti <lucas_possatti@hotmail.com>
2016-06-23 22:25:20 -0300

    Put git word diff aliases

--- git-setup.sh
+++ git-setup.sh
@@ -19,6 +19,8 @@ git config --global push.default simple
 git config --global alias.s 'status --short --branch'
 git config --global alias.d 'diff'
 git config --global alias.dc 'diff --cached'
+git config --global alias.wd 'diff --word-diff'
+git config --global alias.wdc 'diff --word-diff --cached'
 git config --global alias.lg "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 git config --global alias.l '!git lg'
 git config --global alias.hist 'log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
